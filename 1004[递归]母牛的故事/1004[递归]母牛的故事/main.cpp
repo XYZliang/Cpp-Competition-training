3d5cc4057b87be0c5441b576ec69f6c821dcfc66
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-14 16:11:19 +0800

    2020.8.14.1

---
+++ 1004[递归]母牛的故事/1004[递归]母牛的故事/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1004[递归]母牛的故事
+//
+//  Created by 张亮 on 2020/8/13.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num[56]={1};
+    return 0;
+}
---
+++ 1083Hello, world!/1083Hello, world!/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1083Hello, world!
+//
+//  Created by 张亮 on 2020/8/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
