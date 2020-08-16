c822ffb1bf617dc1568636209d3b6e8cdd353bcc
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-16 22:28:48 +0800

    2020.8.16.2

--- 1251: [蓝桥杯2015初赛]星系炸弹/1251: [蓝桥杯2015初赛]星系炸弹/main.cpp
+++ 1251: [蓝桥杯2015初赛]星系炸弹/1251: [蓝桥杯2015初赛]星系炸弹/main.cpp
@@ -6,9 +6,47 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int day[14]={0,31,28,31,30,31,30,31,31,30,31,30,31,99999};
+int y,m,d,a;
+
+void right()
+{
+        day[2]=((y%4==0&&y%100!=0)||y%400==0)?29:28;
+    if(d>day[m])
+    {
+        d-=day[m];
+        m++;
+    }
+    else if(m>12)
+    {
+        y++;
+        m-=12;
+    }
+    else
+    {
+        cout<<y<<"-";
+        if(m<10)
+            cout<<"0"<<m<<"-";
+        else
+            cout<<m<<"-";
+        if(d<10)
+            cout<<"0"<<d<<endl;
+        else
+            cout<<d<<endl;
+        return;
+    }
+    right();
+    return;
+}
+
+int main()
+{
+    while(cin>>y>>m>>d>>a)
+    {
+        d+=a;
+        right();
+    }
     return 0;
 }
