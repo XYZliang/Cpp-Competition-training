f4c8857b53c1fc0cfe9869e2ee66f37861031a5c
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-19 11:20:19 +0800

    2020.8.19.2

---
+++ 1252: [蓝桥杯2015初赛]奇妙的数字/1252: [蓝桥杯2015初赛]奇妙的数字/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1252: [蓝桥杯2015初赛]奇妙的数字
+//
+//  Created by 张亮 on 2020/8/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ 1253: [蓝桥杯2015初赛]牌型种数/1253: [蓝桥杯2015初赛]牌型种数/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1253: [蓝桥杯2015初赛]牌型种数
+//
+//  Created by 张亮 on 2020/8/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
