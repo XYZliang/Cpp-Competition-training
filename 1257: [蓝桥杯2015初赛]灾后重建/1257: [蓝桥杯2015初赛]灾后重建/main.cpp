5ab7efe1be1e2fb2e4a32157399645683c6c1038
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-22 20:33:13 +0800

    2020.8.22.5

---
+++ 1257: [蓝桥杯2015初赛]灾后重建/1257: [蓝桥杯2015初赛]灾后重建/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1257: [蓝桥杯2015初赛]灾后重建
+//
+//  Created by 张亮 on 2020/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
