3d8349d32ac8b757f5f9c630f9b4cef4b5d801e4
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-22 20:29:27 +0800

    2020.8.22.3

---
+++ 1255: [蓝桥杯 2015 初赛] 饮料换购/1255: [蓝桥杯 2015 初赛] 饮料换购/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1255: [蓝桥杯 2015 初赛] 饮料换购
+//
+//  Created by 张亮 on 2020/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
