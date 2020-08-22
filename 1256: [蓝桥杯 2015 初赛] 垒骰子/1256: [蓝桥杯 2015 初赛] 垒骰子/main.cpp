40be5b926b0dcd8e1a09dfb679c8349a7be87653
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-22 20:31:15 +0800

    2020.8.22.4

---
+++ 1256: [蓝桥杯 2015 初赛] 垒骰子/1256: [蓝桥杯 2015 初赛] 垒骰子/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1256: [蓝桥杯 2015 初赛] 垒骰子
+//
+//  Created by 张亮 on 2020/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
