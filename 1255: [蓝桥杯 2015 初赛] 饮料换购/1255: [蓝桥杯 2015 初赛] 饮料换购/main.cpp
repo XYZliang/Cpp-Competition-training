b612fce7a535d5984fad002f505d6038046a91ef
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-08-24 18:03:53 +0800

    2020.8.24.3

--- 1255: [蓝桥杯 2015 初赛] 饮料换购/1255: [蓝桥杯 2015 初赛] 饮料换购/main.cpp
+++ 1255: [蓝桥杯 2015 初赛] 饮料换购/1255: [蓝桥杯 2015 初赛] 饮料换购/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    int n,gai,sum;
+    while(cin>>n)
+    {
+        gai=0;
+        sum=0;
+        do
+        {
+            sum+=n;
+            int m=n+gai;
+            n=m/3;
+            gai=m%3;
+    
+        }while(gai+n>=3);
+        cout<<sum+n<<endl;
+    }
     return 0;
 }
+//101 101 101
+//33 35 134
+//11 13 145
+//4 5 149
+//1 3 150
+//1 0 151
