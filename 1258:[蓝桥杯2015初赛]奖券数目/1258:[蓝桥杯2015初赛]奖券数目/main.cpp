81785fcbabefa84eef31461f05f866350f399baf
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-09-02 20:31:12 +0800

    2020.09.02.1

---
+++ 1258:[蓝桥杯2015初赛]奖券数目/1258:[蓝桥杯2015初赛]奖券数目/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1258:[蓝桥杯2015初赛]奖券数目
+//
+//  Created by 张亮 on 2020/9/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
