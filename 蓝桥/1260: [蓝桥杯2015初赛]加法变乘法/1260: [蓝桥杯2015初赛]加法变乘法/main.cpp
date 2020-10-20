9f15ce45739f814f36f6b85180ec52c5252495f3
张亮 <59312540+XYZliang@users.noreply.github.com>
2020-10-21 00:14:22 +0800

    change plan

---
+++ 蓝桥/1260: [蓝桥杯2015初赛]加法变乘法/1260: [蓝桥杯2015初赛]加法变乘法/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1260: [蓝桥杯2015初赛]加法变乘法
+//
+//  Created by 张亮 on 2020/10/10.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
