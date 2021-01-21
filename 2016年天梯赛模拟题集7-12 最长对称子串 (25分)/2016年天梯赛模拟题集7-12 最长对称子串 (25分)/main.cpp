74bfadaa0e78227401bb81507f29eab6a00e5785
张亮 <59312540+XYZliang@users.noreply.github.com>
2021-01-21 16:28:39 +0800

    2021.1.21.1

---
+++ 2016年天梯赛模拟题集7-12 最长对称子串 (25分)/2016年天梯赛模拟题集7-12 最长对称子串 (25分)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2016年天梯赛模拟题集7-12 最长对称子串 (25分)
+//
+//  Created by 张亮 on 2021/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
