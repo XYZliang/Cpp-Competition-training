9fb7d682141c71377eefbbe963b1bc88cc9f598b
张亮 <59312540+XYZliang@users.noreply.github.com>
2021-02-04 15:44:59 +0800

    2021.2.4.2

--- 2016年天梯赛决赛题集7-3 出租 (20 分)/2016年天梯赛决赛题集7-3 出租 (20 分)/main.cpp
+++ 2016年天梯赛决赛题集7-3 出租 (20 分)/2016年天梯赛决赛题集7-3 出租 (20 分)/main.cpp
@@ -6,9 +6,42 @@
 //
 
 #include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string a;
+    cin>>a;
+    int num[10]={0};
+    int code[10]={0};
+    for(int i=0;i<=9;i++)
+    {
+        num[a[i]-'0']++;
+    }
+    cout<<"int[] arr = new int[]{";
+    bool first=true;
+    int ii=0;
+    for(int i=9;i>=0;i--)
+    {
+        if(num[i])
+        {
+            if (!first)
+                cout<<",";
+            else
+                first=false;
+            code[i]=ii++;
+            cout<<i;
+        }
+    }
+    cout<<"};"<<endl<<"int[] index = new int[]{";
+    first=true;
+    for(int i=0;i<11;i++)
+    {
+            if (!first)
+                cout<<",";
+            else
+                first=false;
+            cout<<code[a[i]-'0'];
+    }
+    cout<<"};"<<endl;
     return 0;
 }
---
+++ 2016年天梯赛决赛题集7-6 一帮一 (15 分)/2016年天梯赛决赛题集7-6 一帮一 (15 分)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2016年天梯赛决赛题集7-6 一帮一 (15 分)
+//
+//  Created by 张亮 on 2021/2/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
